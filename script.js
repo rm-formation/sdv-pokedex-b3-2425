ba57a1328e8ca6cf6c1964a8ba8532bd5e9840bc
Ronan Marret <rm-formation@seolh.fr>
2024-12-09 15:30:02 +0100

    Ajout pokemons dans liste depuis données mockées

--- script.js
+++ script.js
@@ -17,8 +17,41 @@ const pokemons = [
 ];
 
 window.onload = function() {
-    //Code ici
+    const section = document.querySelector("main > section");
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        const divPokemon = creerDivPokemonListe(pokemon);
+        section.append(divPokemon);
+    }
+}
 
+function creerDivPokemonListe(pokemon) {
+    const div = document.createElement("div");
+    div.classList.add("div-pokemon-liste");
 
+    const img = document.createElement("img");
+    img.src = pokemon.imgUrl;
 
+    const spanNom = document.createElement("span");
+    spanNom.innerText = pokemon.nom;
+
+    const spanId = document.createElement("span");
+    spanId.innerText = pokemon.id;
+
+    div.append(img);
+    div.append(spanNom);
+    div.append(spanId);
+
+    return div;
+}
+
+function creerDivPokemonListeVersionInnerHTML(pokemon) {
+    const div = document.createElement("div");
+    div.classList.add("div-pokemon-liste");
+    div.innerHTML = `
+        <img src="${pokemon.imgUrl}">
+        <span>${pokemon.nom}</span>
+        <span>${pokemon.id}</span>
+    `;
+    return div;
 }
