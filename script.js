fd4351873afad4af01304ac130f9c3225775adcf
Ronan Marret <rm-formation@seolh.fr>
2024-12-09 14:16:38 +0100

    Ajout script base + mock premiers pokemons

---
+++ script.js
@@ -0,0 +1,24 @@
+const pokemons = [
+    {
+        id: 1,
+        imgUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/1.png",
+        nom: "Bulbizarre",
+    },
+    {
+        id: 4,
+        imgUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/4.png",
+        nom: "Salamèche",
+    },
+    {
+        id: 7,
+        imgUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/7.png",
+        nom: "Carapuce",
+    },
+];
+
+window.onload = function() {
+    //Code ici
+
+
+
+}
