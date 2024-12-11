114eaf4e2f3326dab84e86cb975fab6ebd5a6eae
Ronan Marret <rm-formation@seolh.fr>
2024-12-11 14:36:35 +0100

    Ajout chargement liste pokemons via api

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
-const pokemons = [
-    {
-        id: 1,
-        imgUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/1.png",
-        nom: "Bulbizarre",
-    },
-    {
-        id: 4,
-        imgUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/4.png",
-        nom: "Salamèche",
-    },
-    {
-        id: 7,
-        imgUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/7.png",
-        nom: "Carapuce",
-    },
-];
-
 const boutonPokedex = document.querySelector(".bouton-pokedex");
-boutonPokedex.addEventListener("click", afficherListePokemons);
+boutonPokedex.addEventListener("click", afficherListePokemonsOptimisee);
 
-function afficherListePokemons() {
+async function afficherListePokemons() {
+    const responsePokemons = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=5000");
+    const json = await responsePokemons.json();
+    const pokemons = json.results;
+
     const conteneur = document.querySelector("main > section .conteneur");
     conteneur.innerHTML = "";
+
     for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        const divPokemon = creerDivPokemonListe(pokemon);
+        const detailsPokemon = pokemons[i];
+        const responsePokemon = await fetch(detailsPokemon.url);
+        const jsonPokemon = await responsePokemon.json();
+        const divPokemon = creerDivPokemonListe(jsonPokemon);
         conteneur.append(divPokemon);
     }
+}
+
+async function afficherListePokemonsOptimisee() {
+    const responsePokemons = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=5000");
+    const json = await responsePokemons.json();
+    const pokemons = json.results;
+
+    const conteneur = document.querySelector("main > section .conteneur");
+    conteneur.innerHTML = "";
+
+    let tableauPromises = [];
+
+    for (let i = 0; i < pokemons.length; i++) {
+        const detailsPokemon = pokemons[i];
+        const promisePokemon = recupererJsonPokemon(detailsPokemon.url);
+        tableauPromises.push(promisePokemon);
+    }
+
+    const tableauJsons = await Promise.all(tableauPromises);
+
+    for (let i = 0; i < tableauJsons.length; i++) {
+        const jsonPokemon = tableauJsons[i];
+        const divPokemon = creerDivPokemonListe(jsonPokemon);
+        conteneur.append(divPokemon);
+    }
+}
+
+async function recupererJsonPokemon(url) {
+    const responsePokemon = await fetch(url);
+    const jsonPokemon = await responsePokemon.json();
+    return jsonPokemon;
 }
 
 function creerDivPokemonListe(pokemon) {
@@ -35,10 +55,12 @@ function creerDivPokemonListe(pokemon) {
     div.setAttribute("data-pokemon-id", pokemon.id);
 
     const img = document.createElement("img");
-    img.src = pokemon.imgUrl;
+    img.src = pokemon.sprites.other["official-artwork"].front_default;
+    //img.src = pokemon.sprites.other.showdown.front_default;
+    //img.src = pokemon.sprites.versions["generation-i"]["red-blue"]["front_transparent"];
 
     const spanNom = document.createElement("span");
-    spanNom.innerText = pokemon.nom;
+    spanNom.innerText = pokemon.name;
 
     const spanId = document.createElement("span");
     spanId.innerText = pokemon.id;
