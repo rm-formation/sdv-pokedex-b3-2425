ab3798b653a7a88ab174c78940cb006a16b9e2f3
Ronan Marret <rm-formation@seolh.fr>
2024-12-12 14:29:40 +0100

    Ajout correction TP favoris

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const boutonPokedex = document.querySelector(".bouton-pokedex");
-boutonPokedex.addEventListener("click", afficherListePokemonsOptimisee);
+let favoris = {};
+const favorisStorage = localStorage.getItem("favoris");
+if (favorisStorage) {
+    favoris = JSON.parse(favorisStorage);
+}
 
-async function afficherListePokemons() {
+const boutonPokedex = document.querySelector(".bouton-pokedex");
+boutonPokedex.addEventListener("click", afficherPokedex);
+
+const boutonFavoris = document.querySelector(".bouton-favoris");
+boutonFavoris.addEventListener("click", afficherFavoris);
+
+/* async function afficherListePokemons() {
     const responsePokemons = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=5000");
     const json = await responsePokemons.json();
     const pokemons = json.results;
@@ -16,29 +25,51 @@ async function afficherListePokemons() {
         const divPokemon = creerDivPokemonListe(jsonPokemon);
         conteneur.append(divPokemon);
     }
-}
+} */
 
-async function afficherListePokemonsOptimisee() {
+async function afficherPokedex() {
     const responsePokemons = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=151");
     const json = await responsePokemons.json();
-    const pokemons = json.results;
-
-    const conteneur = document.querySelector("main > section .conteneur");
-    conteneur.innerHTML = "";
+    const listePokemons = json.results;
 
     let tableauPromises = [];
 
-    for (let i = 0; i < pokemons.length; i++) {
-        const detailsPokemon = pokemons[i];
+    for (let i = 0; i < listePokemons.length; i++) {
+        const detailsPokemon = listePokemons[i];
         const promisePokemon = recupererJsonPokemon(detailsPokemon.url);
         tableauPromises.push(promisePokemon);
     }
 
-    const tableauJsons = await Promise.all(tableauPromises);
+    const pokemons = await Promise.all(tableauPromises);
 
-    for (let i = 0; i < tableauJsons.length; i++) {
-        const jsonPokemon = tableauJsons[i];
-        const divPokemon = creerDivPokemonListe(jsonPokemon);
+    await afficherListePokemons(pokemons);
+}
+
+async function afficherFavoris() {
+    const pokemons = Object.values(favoris);
+    if (pokemons.length > 0) {
+        await afficherListePokemons(pokemons, true);
+    } else {
+        const div = document.createElement("div");
+        div.innerText = "Aucun favoris n'a été enregistré";
+        //NE PAS FAIRE SUR UN VRAI PROJET
+        div.style.whiteSpace = "nowrap";
+        div.style.textAlign = "center";
+        div.style.alignSelf = "center";
+        div.style.gridColumn = "1 / 4";
+        //FIN
+        const conteneur = document.querySelector("main > section .conteneur");
+        conteneur.append(div);
+    }
+}
+
+async function afficherListePokemons(pokemons, pourFavoris = false) {
+    const conteneur = document.querySelector("main > section .conteneur");
+    conteneur.innerHTML = "";
+
+    for (let i = 0; i < pokemons.length; i++) {
+        const jsonPokemon = pokemons[i];
+        const divPokemon = creerDivPokemonListe(jsonPokemon, pourFavoris);
         conteneur.append(divPokemon);
     }
 }
@@ -49,7 +80,7 @@ async function recupererJsonPokemon(url) {
     return jsonPokemon;
 }
 
-function creerDivPokemonListe(pokemon) {
+function creerDivPokemonListe(pokemon, pourFavoris = false) {
     const div = document.createElement("div");
     div.classList.add("div-pokemon-liste");
     div.setAttribute("data-pokemon-id", pokemon.id);
@@ -65,11 +96,32 @@ function creerDivPokemonListe(pokemon) {
     const spanId = document.createElement("span");
     spanId.innerText = pokemon.id;
 
+    const cb = document.createElement("input");
+    cb.type = "checkbox";
+    if (favoris[pokemon.id]) {
+        cb.checked = true;
+    }
+    cb.addEventListener("change", function(e) {
+        if (cb.checked) {
+            favoris[pokemon.id] = pokemon;
+        } else {
+            delete favoris[pokemon.id];
+            if (pourFavoris) {
+                div.remove();
+            }
+        }
+        localStorage.setItem("favoris", JSON.stringify(favoris));
+    });
+
     div.append(img);
     div.append(spanNom);
     div.append(spanId);
+    div.append(cb);
 
-    div.onclick = function() {
+    div.onclick = function(e) {
+        if (e.target instanceof HTMLInputElement) {
+            return;
+        }
         afficherPokemon(pokemon);
     };
 
